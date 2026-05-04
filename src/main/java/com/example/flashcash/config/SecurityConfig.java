b674faa4bafaeec65d6b51dbb6764158ccd70c27
Ilian Metina <ilian.metina83@gmail.com>
2026-05-04 15:24:22 +0200

    adds

--- src/main/java/com/example/flashcash/config/SecurityConfig.java
+++ src/main/java/com/example/flashcash/config/SecurityConfig.java
@@ -1,10 +1,24 @@
 package com.example.flashcash.config;
 
 import com.example.flashcash.repositories.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     private final UserRepository userRepository;
 
+    public SecurityConfig(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+
+    }
 }
